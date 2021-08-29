e032e4cd3aad28f66d95cacd929a7cc5460659bc
bohdeveloper <ohb.seven@gmail.com>
2021-08-29 10:57:45 +0200

    Primer commit

---
+++ src/main/java/com/bohdeveloper/pybank/PybankApplication.java
@@ -0,0 +1,13 @@
+package com.bohdeveloper.pybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PybankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PybankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bohdeveloper/pybank/PybankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bohdeveloper.pybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PybankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
