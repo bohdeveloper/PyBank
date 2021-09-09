6b2f80e999cb28095029e04a11fdfb49347d2f33
bohdeveloper <ohb.seven@gmail.com>
2021-09-09 18:40:46 +0200

    Estructura inicial

--- src/main/java/com/bohdeveloper/pybank/controladores/NavegationController.java
+++ src/main/java/com/bohdeveloper/pybank/controladores/NavegationController.java
@@ -1,19 +1,50 @@
 package com.bohdeveloper.pybank.controladores;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class NavegationController {
     
+/* EMPAQUETAR URL POR SECCIONES ej: /personas/productos */
+
     @GetMapping("/")
-    public String goIndex(){
-        return "index";
+    public String goPrincipal() {
+        return "principal";
+    }
+
+    @GetMapping("/negocios")
+    public String goNegocios() {
+        return "negocios";
+    }
+
+    @GetMapping("/info_registro")
+    public String goInfoRegistroHumano() {
+        return "info_registro";
+    }
+
+    @GetMapping("/info_registro_negocios")
+    public String goInfoRegistroNegocio() {
+        return "info_registro_negocios";
+    }
+
+    @GetMapping("/registro")
+    public String goRegistro() {
+        return "registro";
+    }
+
+    @GetMapping("/registro_negocio")
+    public String goRegistroNegocio() {
+        return "registro_negocio";
     }
 
     @GetMapping("/login")
-    public String goLogin(){
+    public String goLogin() {
+        return "login";
+    }
+
+    @GetMapping("/validar_login")
+    public String goValidarLogin() {
         return "login";
     }
 
